abbcb2f46aad5248c83a469e0eb2e24c4367bd6c
anamazari <USER@DESKTOP-FFKF6L6.(none)>
2024-12-05 21:07:31 -0500

    Aprendiendo JS

---
+++ static/js/pruebas.js
@@ -0,0 +1,7 @@
+let n1 = document.getElementById("num1").value;
+
+let n2 = document.getElementById("num2").value;
+
+document.getElementById("enviar").addEventListener("click",function(){
+    alert(n1+n2)
+})
